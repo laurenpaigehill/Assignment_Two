04c59323ec6cf910b360fb6c4ea135aeb17c637d
“Lauren <laurenhillfreelance@gmail.com>
2023-03-26 17:39:08 +0100

    backspace function added

--- Wordle.js
+++ Wordle.js
@@ -1,9 +1,25 @@
+// letter input function
+
 function placeLetter(letter) {
   const gridItems = document.querySelectorAll(".grid-item");
   for (let i = 0; i < gridItems.length; i++) {
     const gridItem = gridItems[i];
     if (!gridItem.innerHTML) {
       gridItem.innerHTML = letter;
+      gridItem.classList.add("black-border");
+      break;
+    }
+  }
+}
+
+// backspace function
+
+function backspaceClicked() {
+  var inputFields = document.getElementsByClassName("grid-item");
+  for (var i = inputFields.length - 1; i >= 0; i--) {
+    var text = inputFields[i].textContent;
+    if (text.length > 0) {
+      inputFields[i].textContent = text.slice(0, -1);
       break;
     }
   }
